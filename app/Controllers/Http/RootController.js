965609f392166ec3589eb5312cdbe3395a21825a
aacgn <aacgn@cin.ufpe.br>
2018-07-02 17:22:05 -0300

    xml listener

--- app/Controllers/Http/RootController.js
+++ app/Controllers/Http/RootController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const xmlJs = use('xml-js')
+
 class RootController {
     index({response}){
         response.status(200).json({
@@ -8,12 +10,21 @@ class RootController {
         })        
     }
 
-    async receive({request, reponse}){
+    async receive({request, response}){
         const data = request.post()
+
+        var xml = xmlJs.xml2json('<?xml version="1.0" encoding="utf-8"?>' +
+        '<note importance="high" logged="true">' +
+        '    <title>Happy</title>' +
+        '    <todo>Work</todo>' +
+        '    <todo>Play</todo>' +
+        '</note>', {compact: true, spaces: 4})
+
+        console.info(xml)
 
         response.status(200).json({
             message: 'Successfully receive data.',
-            data: researcher
+            data: {}
         })
         
     }
