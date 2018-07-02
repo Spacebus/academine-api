63ecc3ee716ac59a7b1eb277e3d3d0cfa45af74c
aacgn <aacgn@cin.ufpe.br>
2018-07-02 18:55:49 -0300

    CORS

--- start/app.js
+++ start/app.js
@@ -10,12 +10,14 @@
 | provider here.
 |
 */
+
 const providers = [
   '@adonisjs/framework/providers/AppProvider',
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/bodyparser/providers/BodyParserProvider',
   '@adonisjs/cors/providers/CorsProvider',
-  '@adonisjs/lucid/providers/LucidProvider'
+  '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/cors/providers/CorsProvider'
 ]
 
 /*
